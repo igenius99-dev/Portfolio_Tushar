1961f2368cf9007a87a0b47ad55334a241c622cb
igenius99-dev <tusharsachan06@gmail.com>
2025-10-21 14:15:16 -0700

    fixing minor bugs

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function ContactPage() {
   return (
-    <article className="mt-8 flex flex-col gap-8 pb-16">
+    <article className="mt-24 flex flex-col gap-8 pb-24">
       <motion.h1 
         className="title"
         initial={{ opacity: 0, y: 30 }}
